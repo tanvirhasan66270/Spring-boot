3fa133ccff1e56c44d928ee5297eadab6bc295e2
Md. Tanvir <tanvirhasan66270@gmail.com>
2026-06-09 18:54:44 +0600

    add

---
+++ SCM/src/main/java/com/example/SCM/entity/Division.java
@@ -0,0 +1,29 @@
+package com.example.SCM.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="division")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Division {
+
+
+    @Id
+     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    long id;
+
+    @Column(unique=true)
+    String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+
+}
