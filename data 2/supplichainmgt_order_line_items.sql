3c74f8fa33b3d478d8de5a116442bf61e483850e
Md. Tanvir <tanvirhasan66270@gmail.com>
2026-08-20 03:14:22 +0600

    add

--- data 2/supplichainmgt_order_line_items.sql
+++ data 2/supplichainmgt_order_line_items.sql
@@ -36,7 +36,7 @@ CREATE TABLE `order_line_items` (
   KEY `FKpogrqnaru5vyemp3e9c997ag3` (`product_id`),
   CONSTRAINT `FKk6hajnfcni3fw6vmupsypd7hh` FOREIGN KEY (`order_id`) REFERENCES `customer_orders` (`id`),
   CONSTRAINT `FKpogrqnaru5vyemp3e9c997ag3` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `order_line_items` (
 
 LOCK TABLES `order_line_items` WRITE;
 /*!40000 ALTER TABLE `order_line_items` DISABLE KEYS */;
-INSERT INTO `order_line_items` VALUES (1,0.06,1000,2,NULL,500,1,10),(2,120,500,1,NULL,500,1,2),(3,0.06,1000,2,NULL,500,2,10),(4,120,500,1,NULL,500,2,2),(7,91,64000,2,'Please pack with extra protection wrap.',32000,4,1),(8,95,72000,1,'Check the machine gear antes before dispatch.',72000,4,4),(9,910,640000,20,'zdfvgbdb',32000,5,1),(10,764,480000,20,'vdsfdsf',24000,6,3);
+INSERT INTO `order_line_items` VALUES (1,0.06,1000,2,NULL,500,1,10),(2,120,500,1,NULL,500,1,2),(3,0.06,1000,2,NULL,500,2,10),(4,120,500,1,NULL,500,2,2),(7,91,64000,2,'Please pack with extra protection wrap.',32000,4,1),(8,95,72000,1,'Check the machine gear antes before dispatch.',72000,4,4),(12,764,480000,20,'vdsfdsf',24000,6,3),(13,910,640000,20,'zdfvgbdb',32000,5,1);
 /*!40000 ALTER TABLE `order_line_items` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-19  4:14:51
+-- Dump completed on 2026-08-20  3:13:54
